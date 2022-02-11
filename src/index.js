20bdf7f90085e594f8db00ff4e7105dc9b7fc1cf
unknown <cornbee80014@gmail.com>
2022-02-11 11:05:39 +0900

    project init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+  // public 폴더의 index.html 속 <div id='root'> 에다가 위의 값을 넣음
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
