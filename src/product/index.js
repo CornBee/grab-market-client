a40d0f9ba39692c9f90f7443f4b51402fb63a4bb
CornBee <cornbee80014@gmail.com>
2022-02-17 11:55:00 +0900

    antd 기본 적용

--- src/product/index.js
+++ src/product/index.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
   const { id } = useParams();
@@ -17,8 +18,30 @@ function ProductPage() {
         console.error(error);
       });
   }, []);
-  console.log(product);
-  return <h1>상품 상세 페이지 {id} 상품</h1>;
+
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+        {/* /는 이 프로젝트 안 임을 뜻함 */}
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        {/* public 적는 것은 생략할 수 있음 */}
+        <span>{product.seller}</span>
+      </div>
+      <div id="contents-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createAt">2022년 2월 17일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
